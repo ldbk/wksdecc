77b99b5c94e619e57cb09ca12f8e2f59712792fc
Laurent Dubroca <laurent.dubroca@gmail.com>
2017-05-30 11:03:11 +0200

    improvement

--- R/generatetab.R
+++ R/generatetab.R
@@ -8,7 +8,7 @@
 #' @return a data frame 
 #' @author Laurent Dubroca
 #' @name createdf 
-#' 
+#' @export
 createdf<-function(name="Design",nbrow=100){
 	pathrdbformat<-system.file("extdata","RDB_CS_Data_Model_v1.3.xlsx",package="WKSDECC")
 	aa<-readxl::read_excel(pathrdbformat,sheet=name)
@@ -18,3 +18,22 @@ createdf<-function(name="Design",nbrow=100){
 	names(bb)<-as.vector(t(aa$`R-Object name`))
 	return(bb)
 }
+#' Pick values from a data frame to another if the names match
+#' 
+#'
+#' @param tab1 the dataframe to which the values are copied
+#' @param tab2 the dataframe from which the values are coming from
+#' @return a data frame 
+#' @author Laurent Dubroca
+#' @name pickvalue 
+#' @export
+pickvalue<-function(tab1,tab2){
+	for(i in 1:ncol(tab1)){
+			if(any(names(tab1)[i]%in%names(tab2))){
+			#print(names(tab1)[i])
+			tab1[,i]<-tab2[,names(tab2)==names(tab1)[i]]
+			}
+	}
+	return(tab1)
+}
+
---
+++ R/wksdecc.R
@@ -0,0 +1,10 @@
+#' WKSDECC functions and tools 
+#'
+#' Some functions coded during WKSDECC
+#'
+#' Some details here.
+#'
+#' @docType package
+#' @name WKSDECC 
+NULL
+
