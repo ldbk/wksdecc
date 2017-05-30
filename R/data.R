19d62e939d53339bc18f6221b7f6222d9baf429c
Laurent Dubroca <laurent.dubroca@gmail.com>
2017-05-30 10:35:03 +0200

    first commit

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Format definition for fishframe 
+#'
+#' The format used by fishframe
+#'
+#' @docType data
+#' @keywords data
+#' @name format_definition_fishframe
+#' @format A format definition in xlsx
+NULL
+#' Format definition for a new sampling format 
+#'
+#' The format dev during WKSDECC and before 
+#'
+#' @docType data
+#' @keywords data
+#' @name RDB_CS_Data_Model_v1.3
+#' @format A format definition in xlsx
+NULL
---
+++ README.md
@@ -0,0 +1,17 @@
+# WKSDECC tools
+
+A package to export fishframe data to a new sampling format and more.
+
+# Installation
+
+In a R console:
+
+`install.packages("devtools")`
+
+`library(devtools)`
+
+`install_github("ldbk/wksdecc")`
+
+# Description
+
+Some words.
